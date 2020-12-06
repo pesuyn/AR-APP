d85d72cf6069bbe36f3313564e4c8021c5965c1d
Thuy <pesuyn444@gmail.com>
2020-12-06 17:44:43 +0700

    add model and update code

--- app/src/main/java/com/example/ar_app/MainActivity.java
+++ app/src/main/java/com/example/ar_app/MainActivity.java
@@ -22,40 +22,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CustomArFragment customArFragment = (CustomArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
-            ModelRenderable
-                    .builder()
-                    .setSource(this, R.raw.fox_face)
-                    .build()
-                    .thenAccept(renderable ->
-                            {
-                                modelRenderable = renderable;
-                                modelRenderable.setShadowCaster(false);
-                                modelRenderable.setShadowReceiver(false);
-                            });
+        CustomArFragment CustomArFragment = (CustomArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
 
-            Texture
-                    .builder().setSource(this, R.drawable.fox_face_mesh_texture)
-                    .build()
-                    .thenAccept(texture -> this.texture = texture);
-            customArFragment.getArSceneView().setCameraStreamRenderPriority(Renderable.RENDER_PRIORITY_FIRST);
+        ModelRenderable.builder()
+                .setSource(this, R.raw.fox_face)
+                .build()
+                .thenAccept(renderable -> {
+                    modelRenderable = renderable;
 
-            customArFragment.getArSceneView().getScene().addOnUpdateListener(frameTime -> {
-                if (modelRenderable == null || texture == null)
+                    modelRenderable.setShadowReceiver(false);
+                    modelRenderable.setShadowCaster(false);
+                });
+
+        Texture.builder()
+                .setSource(this, R.drawable.mask2)
+                .build()
+                .thenAccept(texture -> this.texture =texture );
+
+        CustomArFragment.getArSceneView().setCameraStreamRenderPriority(Renderable.RENDER_PRIORITY_FIRST);
+
+        CustomArFragment.getArSceneView().getScene().addOnUpdateListener(frameTime -> {
+
+            if(modelRenderable == null && texture == null)
+                return;
+
+            Frame frame = CustomArFragment.getArSceneView().getArFrame();
+
+            Collection<AugmentedFace> augmentedFaces = frame.getUpdatedTrackables(AugmentedFace.class);
+
+            for (AugmentedFace augmentedFace : augmentedFaces) {
+                if(isAdded)
                     return;
-                Frame frame = customArFragment.getArSceneView().getArFrame();
-                Collection<AugmentedFace> augmentedFaces = frame.getUpdatedTrackables(AugmentedFace.class);
-                for (AugmentedFace augmentedFace : augmentedFaces) {
-                    if (isAdded)
-                        return;
-                    AugmentedFaceNode augmentedFaceNode = new AugmentedFaceNode(augmentedFace);
-                    augmentedFaceNode.setParent(customArFragment.getArSceneView().getScene());
-                    augmentedFaceNode.setFaceRegionsRenderable(modelRenderable);
-                    augmentedFaceNode.setFaceMeshTexture(texture);
-                    isAdded = true;
 
-                }
-            });
+                AugmentedFaceNode augmentedFaceNode = new AugmentedFaceNode(augmentedFace);
+                augmentedFaceNode.setParent(CustomArFragment.getArSceneView().getScene());
+                augmentedFaceNode.setFaceRegionsRenderable(modelRenderable);
+                augmentedFaceNode.setFaceMeshTexture(texture);
+
+                isAdded = true;
+            }
+
+
+        });
 
     }
 }
