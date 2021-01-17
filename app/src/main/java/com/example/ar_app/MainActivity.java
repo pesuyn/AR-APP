c08c52a11370dbcf81e98841237c0ce80e8456b6
Viet <martgold321@gmail.com>
2021-01-18 00:35:07 +0700

    change

--- app/src/main/java/com/example/ar_app/MainActivity.java
+++ app/src/main/java/com/example/ar_app/MainActivity.java
@@ -1,61 +1,359 @@
 package com.example.ar_app;
 
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
+import com.google.ar.core.Anchor;
 import com.google.ar.core.AugmentedFace;
+import com.google.ar.core.Camera;
 import com.google.ar.core.Frame;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.rendering.Renderable;
 import com.google.ar.sceneform.rendering.Texture;
+import com.google.ar.sceneform.rendering.ViewRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.AugmentedFaceNode;
+import com.google.ar.sceneform.ux.BaseArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
+import java.util.Date;
+import java.util.concurrent.CompletableFuture;
 
 public class MainActivity extends AppCompatActivity {
+
     private ModelRenderable modelRenderable;
     private Texture texture;
     private boolean isAdded = false;
+    private ImageButton no_filter_btn, mask2, op, snap;
+    private int typeFace = 0;
+    private ArFragment arFragment;
+    public static final int CAMERA_PERM_CODE = 101;
+    public static final int CAMERA_REQUEST_CODE = 102;
+    public static final int GALLERY_REQUEST_CODE = 105;
+    String currentPhotoPath;
+    ImageView selectedImage;
+
+
+    View arrayView[];
+    ViewRenderable name_object;
+    int selected = 1;
+//    private ModelRenderable
+
+
+
+
+    private static final int MASK[] = {
+            R.id.no_filter,
+            R.id.hair,
+            R.id.op,
+            R.id.snap,
+            R.id.glasses2,
+            R.id.glasses3,
+            R.id.glasses4,
+            R.id.glasses5,
+            R.id.mask,
+            R.id.mask2,
+            R.id.mask3,
+            R.id.dog,
+            R.id.cat2
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CustomArFragment customArFragment = (CustomArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
-            ModelRenderable
-                    .builder()
-                    .setSource(this, R.raw.fox_face)
-                    .build()
-                    .thenAccept(renderable ->
-                            {
-                                modelRenderable = renderable;
-                                modelRenderable.setShadowCaster(false);
-                                modelRenderable.setShadowReceiver(false);
-                            });
 
-            Texture
-                    .builder().setSource(this, R.drawable.fox_face_mesh_texture)
-                    .build()
-                    .thenAccept(texture -> this.texture = texture);
-            customArFragment.getArSceneView().setCameraStreamRenderPriority(Renderable.RENDER_PRIORITY_FIRST);
 
-            customArFragment.getArSceneView().getScene().addOnUpdateListener(frameTime -> {
-                if (modelRenderable == null || texture == null)
-                    return;
-                Frame frame = customArFragment.getArSceneView().getArFrame();
-                Collection<AugmentedFace> augmentedFaces = frame.getUpdatedTrackables(AugmentedFace.class);
-                for (AugmentedFace augmentedFace : augmentedFaces) {
-                    if (isAdded)
-                        return;
-                    AugmentedFaceNode augmentedFaceNode = new AugmentedFaceNode(augmentedFace);
-                    augmentedFaceNode.setParent(customArFragment.getArSceneView().getScene());
-                    augmentedFaceNode.setFaceRegionsRenderable(modelRenderable);
-                    augmentedFaceNode.setFaceMeshTexture(texture);
-                    isAdded = true;
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ARFragment);
+        op = (ImageButton) findViewById(R.id.op);
+        snap = (ImageButton) findViewById(R.id.snap);
+        mask2 = (ImageButton) findViewById(R.id.mask2);
+        no_filter_btn = (ImageButton) findViewById(R.id.no_filter);
 
-                }
-            });
+        ImageButton no_filter = (ImageButton) findViewById(R.id.no_filter);
+        no_filter.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v2) {
+                findViewById(MASK[typeFace]).setBackgroundResource(R.drawable.round_background);
+                typeFace = 0;
+                findViewById(MASK[typeFace]).setBackgroundResource(R.drawable.round_background_select);
+            }
+        });
+
+        ImageButton op = (ImageButton) findViewById(R.id.op);
+        op.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                findViewById(MASK[typeFace]).setBackgroundResource(R.drawable.round_background);
+                typeFace = 2;
+                findViewById(MASK[typeFace]).setBackgroundResource(R.drawable.round_background_select);
+                getFilter();
+            }
+        });
+
+        ImageButton mask2 = (ImageButton) findViewById(R.id.mask2);
+        mask2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                findViewById(MASK[typeFace]).setBackgroundResource(R.drawable.round_background);
+                typeFace = 9;
+                findViewById(MASK[typeFace]).setBackgroundResource(R.drawable.round_background_select);
+
+                getFilter1();
+            }
+        });
+
+        ImageButton snap = (ImageButton) findViewById(R.id.snap);
+        snap.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v1) {
+                findViewById(MASK[typeFace]).setBackgroundResource(R.drawable.round_background);
+                typeFace = 3;
+                findViewById(MASK[typeFace]).setBackgroundResource(R.drawable.round_background_select);
+                getFilter1();
+
+            }
+        });
+
+        ImageButton camera = (ImageButton) findViewById(R.id.camera);
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                askCameraPermissions();
+            }
+        });
 
     }
+
+
+    private void getFilter() {
+        CustomArFragment customARFragment = (CustomArFragment) getSupportFragmentManager().findFragmentById(R.id.ARFragment);
+
+        final CompletableFuture<Void> voidCompletableFuture = ModelRenderable.builder()
+                .setSource(this, R.raw.fox_face)
+                .build()
+                .thenAccept(renderable -> {
+                    modelRenderable = renderable;
+
+                    modelRenderable.setShadowReceiver(false);
+                    modelRenderable.setShadowCaster(false);
+                });
+
+        Texture.builder()
+                .setSource(this, R.drawable.snap)
+                .build()
+                .thenAccept(texture -> this.texture =texture );
+
+        customARFragment.getArSceneView().setCameraStreamRenderPriority(Renderable.RENDER_PRIORITY_FIRST);
+
+        customARFragment.getArSceneView().getScene().addOnUpdateListener(frameTime -> {
+
+            if(modelRenderable == null && texture == null)
+                return;
+
+            Frame frame = customARFragment.getArSceneView().getArFrame();
+
+            Collection<AugmentedFace> augmentedFaces = frame.getUpdatedTrackables(AugmentedFace.class);
+
+            for (AugmentedFace augmentedFace : augmentedFaces) {
+                if(isAdded)
+                    return;
+
+                AugmentedFaceNode augmentedFaceNode = new AugmentedFaceNode(augmentedFace);
+                augmentedFaceNode.setParent(customARFragment.getArSceneView().getScene());
+                augmentedFaceNode.setFaceRegionsRenderable(modelRenderable);
+                augmentedFaceNode.setFaceMeshTexture(texture);
+
+                isAdded = true;
+            }
+
+
+        });
+    }
+
+    private void getFilter1() {
+        CustomArFragment customARFragment = (CustomArFragment) getSupportFragmentManager().findFragmentById(R.id.ARFragment);
+
+        ModelRenderable.builder()
+                .setSource(this, R.raw.fox_face)
+                .build()
+                .thenAccept(renderable -> {
+                    modelRenderable = renderable;
+
+                    modelRenderable.setShadowReceiver(false);
+                    modelRenderable.setShadowCaster(false);
+                });
+
+        Texture.builder()
+                .setSource(this, R.drawable.mask2)
+                .build()
+                .thenAccept(texture -> this.texture =texture );
+
+        customARFragment.getArSceneView().setCameraStreamRenderPriority(Renderable.RENDER_PRIORITY_FIRST);
+
+        customARFragment.getArSceneView().getScene().addOnUpdateListener(frameTime -> {
+
+            if(modelRenderable == null && texture == null)
+                return;
+
+            Frame frame = customARFragment.getArSceneView().getArFrame();
+
+            Collection<AugmentedFace> augmentedFaces = frame.getUpdatedTrackables(AugmentedFace.class);
+
+            for (AugmentedFace augmentedFace : augmentedFaces) {
+                if(isAdded)
+                    return;
+
+                AugmentedFaceNode augmentedFaceNode = new AugmentedFaceNode(augmentedFace);
+                augmentedFaceNode.setParent(customARFragment.getArSceneView().getScene());
+                augmentedFaceNode.setFaceRegionsRenderable(modelRenderable);
+                augmentedFaceNode.setFaceMeshTexture(texture);
+
+                isAdded = true;
+            }
+
+
+        });
+        customARFragment.isRemoving();
+    }
+
+
+    private void askCameraPermissions() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
+        }else {
+            dispatchTakePictureIntent();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == CAMERA_PERM_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                dispatchTakePictureIntent();
+            }else {
+                Toast.makeText(this, "Camera Permission is Required to Use camera.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                File f = new File(currentPhotoPath);
+                selectedImage.setImageURI(Uri.fromFile(f));
+                Log.d("tag", "ABsolute Url of Image is " + Uri.fromFile(f));
+
+                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                Uri contentUri = Uri.fromFile(f);
+                mediaScanIntent.setData(contentUri);
+                this.sendBroadcast(mediaScanIntent);
+            }
+
+        }
+
+        if (requestCode == GALLERY_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                Uri contentUri = data.getData();
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String imageFileName = "JPEG_" + timeStamp + "." + getFileExt(contentUri);
+                Log.d("tag", "onActivityResult: Gallery Image Uri:  " + imageFileName);
+                selectedImage.setImageURI(contentUri);
+            }
+
+        }
+
+
+    }
+
+    private String getFileExt(Uri contentUri) {
+        ContentResolver c = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(((ContentResolver) c).getType(contentUri));
+    }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+//        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "Viet",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
 }
